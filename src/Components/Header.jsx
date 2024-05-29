06b9366d1f720967e7d4d1dff189eef591ef3e2e
theatashaikh <atashaikh2000@gmail.com>
2024-05-29 08:08:35 +0530

    Modify app name a little bit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h1>React-Translate-App</h1>
+      <h1 style={{ fontWeight: "lighter" }}>ReactTranslateApp</h1>
       <p>Wow!!!, What an App.</p>
     </header>
   );
